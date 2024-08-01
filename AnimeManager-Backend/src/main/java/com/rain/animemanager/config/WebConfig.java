b5fad8624d06cf1313ccb76614d0034ab7780b23
jellyrain <840519987@qq.com>
2024-08-02 00:28:51 +0800

    启动跨域

---
+++ AnimeManager-Backend/src/main/java/com/rain/animemanager/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.rain.animemanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web 配置
+ *
+ * @author rain
+ * @date 2024/08/01
+ */
+@Configuration
+public class WebConfig {
+
+    /**
+     * Web MVC 配置器 （支持跨域）
+     *
+     * @return {@link WebMvcConfigurer }
+     */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
